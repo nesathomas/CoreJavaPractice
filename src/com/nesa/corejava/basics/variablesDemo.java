d0889f1804af74128584999377035538238b8518
nesathomas <nesathomas123@gmail.com>
2026-01-17 17:08:32 -0600

    Lesson 1: Variables and data types

---
+++ src/com/nesa/corejava/basics/variablesDemo.java
@@ -0,0 +1,32 @@
+package com.nesa.corejava.basics;
+
+public class variablesDemo {
+
+
+	public static void main (String[] args) {
+		
+		String name = "Amy";
+		int age = 25;
+		double salary = 100000;
+		float bmi = 10.2f;
+		char gender = 'f';
+		short number = 23;
+		long num = 3445345L;
+		boolean isEmployed = true;
+//		final int age2 = 26;
+//		age2 = 27;
+		
+		System.out.println("The person nameed "+name+
+				" age "+age+
+				" earns salary of $"+salary+
+				" has a bmi of "+bmi+
+				" is a "+gender+
+				" belongs to number "+number+
+				" out of "+num+
+				" and her work status is "+isEmployed);
+		
+		
+	}
+	
+	
+}
